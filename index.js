e2f3e60274f9ad8b86c7a4f41e1a04100342d81b
Dibyasha207 <Devlop@123>
2026-05-28 17:26:35 +0530

    first deployment project

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-console.log("hello jenkins");
-console.log("hello jenkins again");
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+
+   res.write("Hello from Jenkins Auto Deployment");
+
+   res.end();
+
+});
+
+server.listen(3000, '0.0.0.0', () => {
+
+   console.log("Server running on port 3000");
+
+});
