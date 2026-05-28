1a5578222f2e5b16b7f38cbac86a4399dd66e803
Dibyasha207 <Devlop@123>
2026-05-28 18:22:02 +0530

    testing auto deploy

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
 
-   res.write("New Deployment Working");
+   res.write("Auto Deployment Working Successfully");
 
    res.end();
 
