9dcb8cae6309cec4cd42bf25ec3263988bff360e
Dibyasha207 <Devlop@123>
2026-05-28 12:59:58 +0530

    added

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-const http = require('http');
-
-const server = http.createServer((req,res)=>{
-   res.writeHead(200,{'Content-Type':'text/plain'});
-   res.end("Hello from PM2 deployed app");
-});
-
-server.listen(3000,()=>{
-   console.log("Server running on port 3000");
-});
+console.log("hello jenkins");
